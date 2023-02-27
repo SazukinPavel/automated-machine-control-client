8431df1b1ba28db2814fecca194a93b2a4681f51
SazukinPavel <sazukinpavelal@gmail.com>
2023-02-27 22:20:10 +0300

    add route paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,23 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Login from "@/views/Login.vue";
+import Admin from "@/views/Admin.vue";
+import Departament from "@/views/Departament.vue";
 
-const routes = [{ path: "/login", name: "Login", component: Login }];
+const routes = [
+  { path: "/login", name: "Login", component: Login },
+  {
+    path: "/departament",
+    name: "Departament",
+    component: Departament,
+    meta: { isUserRoute: true },
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+    meta: { isAdminRoute: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
