bfb1cfd955fa5134aa7616c8d2e5855a8136de8f
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-16 00:22:21 +0300

    add search component

---
+++ src/components/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-text-field
+    density="compact"
+    class="mr-2"
+    label="Поиск.."
+    :model-value="props.modelValue"
+    @input="emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+const props = defineProps({ modelValue: { type: String, required: true } });
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped></style>
