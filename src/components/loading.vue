935bc3dd8b2c20722111664b8004648340e1d01c
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-16 00:22:53 +0300

    add loafing component

---
+++ src/components/loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="d-flex justify-center align-center"
+    style="width: 100%; height: 100vh"
+  >
+    <v-progress-circular color="primary" indeterminate size="75" width="8" />
+  </div>
+</template>
