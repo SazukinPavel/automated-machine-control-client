a0f4d63d1cdc0f10231359bb4d2b502fe309dbf5
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-16 00:25:14 +0300

    add views

---
+++ src/views/AddMachine.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-container
+    v-if="!isEditLoading"
+    style="max-width: 1000px"
+    class="pa-4 ma-auto mt-xl-15"
+  >
+    <v-card-title class="text-center">
+      {{ isEdit ? "Редактирование станка" : "Добавление станка" }}</v-card-title
+    >
+    <v-form ref="machineForm">
+      <v-text-field
+        class="my-2"
+        :rules="[required]"
+        variant="outlined"
+        label="Название"
+        color="primary"
+        v-model="addMachineDto.name"
+      />
+      <v-textarea
+        class="my-2"
+        :rules="[required]"
+        variant="outlined"
+        label="Описание"
+        color="primary"
+        v-model="addMachineDto.description"
+      />
+    </v-form>
+    <div class="d-flex justify-end align-center">
+      <v-btn class="mx-5" :disabled="isLoading" @click="goBack">Назад</v-btn>
+      <v-btn class="mx-5" :loading="isLoading" @click="addMachine">{{
+        props.isEdit ? "Обновить" : "Добавить"
+      }}</v-btn>
+    </div>
+  </v-container>
+  <loading v-else />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, defineProps } from "vue";
+import { useRoute } from "vue-router";
+import useValidators from "@/hooks/useValidators";
+import AddMachineDto from "@/types/dto/machines/AddMachineDto";
+import { useStore } from "vuex";
+import useNavigateTo from "@/hooks/useNavigateTo";
+import api from "@/api";
+import Loading from "@/components/loading.vue";
+import UpdateMachineDto from "@/types/dto/machines/UpdateMachineDto";
+import Machine from "@/types/Machine";
+
+const props = defineProps({ isEdit: { type: Boolean, default: false } });
+
+const route = useRoute();
+const store = useStore();
+const { required } = useValidators();
+const { goTo, goBack } = useNavigateTo();
+
+const addMachineDto = ref<AddMachineDto>({
+  name: "",
+  description: "",
+});
+const machineForm = ref<any | null>(null);
+const realMachine = ref<Machine | null>(null);
+const isLoading = ref(false);
+const isEditLoading = ref(false);
+const departament = ref();
+
+const addMachine = async () => {
+  if (!(await machineForm.value?.validate().valid)) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    if (props.isEdit) {
+      const updateMachineDto = { ...addMachineDto.value } as UpdateMachineDto;
+      await api.machines.update(updateMachineDto);
+    } else {
+      await store.dispatch("machines/add", {
+        ...addMachineDto.value,
+        departamentId: departament.value,
+      });
+    }
+
+    store.commit("snackbar/showSnackbarSuccess", {
+      message: `Станок успешно ${props.isEdit ? "изменён" : "добавлен"}`,
+    });
+    goTo("Departament", { id: departament.value });
+  } catch (e) {
+    console.log(e);
+    store.commit("snackbar/showSnackbarError", {
+      message: `Произошла ошибка при ${
+        props.isEdit ? "изменение" : "добавление"
+      } станка`,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(async () => {
+  departament.value = route.query.departament?.toString() || "";
+  if (props.isEdit) {
+    isEditLoading.value = true;
+    try {
+      const response = await api.machines.getById(route.params.id.toString());
+      addMachineDto.value = { ...response.data };
+      realMachine.value = response.data;
+      departament.value = realMachine.value.departament?.id;
+    } catch {
+      store.commit("snackbar/showSnackbarSuccess", {
+        message: "Произошла ошибка при получение станка(",
+      });
+      goBack();
+    } finally {
+      isEditLoading.value = false;
+    }
+  }
+});
+</script>
+
+<style scoped></style>
--- src/views/Departament.vue
+++ src/views/Departament.vue
@@ -1,287 +1,98 @@
 <template>
-  <v-card color="primary" variant="flat" class="ma-0 pa-0" width="100vw">
-    <v-tabs v-model="tab" align-tabs="title">
-      <v-tab value="machines"> Машины </v-tab>
-      <v-tab value="defect"> Дефекты </v-tab>
-    </v-tabs>
-    <v-window v-model="tab">
-      <v-window-item value="machines">
-        <v-dialog persistent v-model="defectDialog" width="auto">
-          <v-card class="px-15 py-10">
-            <v-card-title> Новый дефект </v-card-title>
-            <v-form ref="defectForm">
-              <v-text-field
-                v-model="addDefectDto.name"
-                label="Название:"
-                :rules="requiredRule"
-              ></v-text-field>
-              <v-textarea
-                v-model="addDefectDto.description"
-                label="Описание:"
-                :rules="requiredRule"
-              ></v-textarea>
-            </v-form>
-
-            <v-card-actions class="d-flex justify-center">
-              <v-btn
-                variant="outlined"
-                color="primary"
-                @click="closeDefectDialog"
-                >Закрыть</v-btn
-              >
-              <v-btn
-                variant="outlined"
-                class="mx-5"
-                color="primary"
-                :loading="isAddDefectLoading"
-                @click="addDefect"
-                >Добавить</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-card flat>
-          <v-text-field v-model="searchParam" label="Найти" />
-          <v-expansion-panels
-            v-model="selectedMachine"
-            class="mt-10"
-            variant="accordion"
-          >
-            <v-expansion-panel
-              v-for="machine in filtredMachines"
-              :key="machine.id"
-              :value="machine.id"
-            >
-              <v-expansion-panel-title>
-                <v-row>
-                  <v-col cols="11">
-                    <span>{{ machine.name }}</span>
-                  </v-col>
-                  <v-col cols="1">
-                    <v-chip :color="machine.isActive ? 'green' : 'red'">{{
-                      machine.isActive ? "ON" : "OFF"
-                    }}</v-chip>
-                  </v-col>
-                </v-row>
-              </v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <v-card flat>
-                  <v-card-title class="text-h5 text-center"
-                    >Описание</v-card-title
-                  >
-                  <v-card-text class="text-h6 mb-5">{{
-                    machine.description
-                  }}</v-card-text>
-                  <v-divider class="border-opacity-100" color="primary" />
-                  <div class="d-flex jsutify-start my-7">
-                    <v-btn @click="refreshDefects" class="mx-5" color="primary"
-                      >Обновить</v-btn
-                    >
-                    <v-btn
-                      :loading="isAddDefectLoading"
-                      @click="openDefectDialog(machine.id)"
-                      class="mx-5"
-                      color="primary"
-                      >Добавить</v-btn
-                    >
-                  </div>
-                  <v-data-table
-                    :loading="isDefectLoading"
-                    :items="defects"
-                    :headers="defectsHeaders"
-                    class="elevation-1 table"
-                  >
-                    <template v-slot:[`item.isResolved`]="{ item }">
-                      <span>{{ item.raw.isResolved ? "Да" : "Нет" }}</span>
-                    </template>
-                    <template v-slot:[`item.createdAt`]="{ value }">
-                      <span>{{
-                        moment(value).format("DD-MM-YY hh-mm-ss")
-                      }}</span>
-                    </template>
-                    <template v-slot:[`item.updatedAt`]="{ value }">
-                      <span>{{
-                        moment(value).format("DD-MM-YY hh-mm-ss")
-                      }}</span>
-                    </template>
-                    <template v-slot:[`item.actions`]="{ item }">
-                      <div class="d-flex justify-center align-center">
-                        <v-btn
-                          color="primary"
-                          variant="outlined"
-                          v-if="!item.raw.isResolved"
-                          @click="closeDefect(machine.id, item.raw.id)"
-                          >Закрыть</v-btn
-                        >
-                      </div>
-                    </template>
-                  </v-data-table>
-                </v-card>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-card>
-      </v-window-item>
-    </v-window>
-  </v-card>
+  <div v-if="!isLoading">
+    <v-row class="ma-4" dense="dense">
+      <v-col style="min-width: 150px" cols="3">
+        <search v-model="searchParam" />
+      </v-col>
+      <v-col cols="3" style="min-width: 150px">
+        <v-select
+          v-model="selectedState"
+          :items="stateItems"
+          label="Состояние"
+          density="compact"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-btn
+          v-if="id"
+          class="mt-1"
+          append-icon="mdi-plus"
+          :to="{ name: 'AddMachine', query: { departament: id } }"
+          >Добавить
+        </v-btn>
+      </v-col>
+    </v-row>
+    <machine-card
+      v-for="machine in filtredMachines"
+      :key="machine.id"
+      :machine="machine"
+    />
+  </div>
+  <loading v-else></loading>
 </template>
 
 <script setup lang="ts">
 import { computed, onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 import { useStore } from "vuex";
-import moment from "moment";
 import Machine from "@/types/Machine";
-import AddDefectDto from "@/types/dto/defects/AddDefectDto";
-import api from "@/api";
-import UpdateDefectDto from "@/types/dto/defects/UpdateDefectDto";
+import MachineCard from "@/components/machineCard.vue";
+import Search from "@/components/search.vue";
+import useDateFormater from "@/hooks/useDateFormater";
+import Loading from "@/components/loading.vue";
 
+const route = useRoute();
 const store = useStore();
-const tab = ref(0);
-const requiredRule: any = [(val: string) => !!val || "Поле обязательно!"];
+const { formatDateTime } = useDateFormater();
+
+const isLoading = ref(false);
 const searchParam = ref("");
+const selectedState = ref("");
+const stateItems = ref([
+  { title: "С дефектами", value: true },
+  { title: "Без дефектов", value: false },
+  { title: "Все", value: "" },
+]);
 
-onMounted(() => {
-  store.dispatch("machines/fetch");
-});
+const id = computed(() => route.params.id);
 
-const selectedMachine = ref<string>("");
-
-const machines = computed<Machine[]>(() => store.getters["machines/machines"]);
-// const isMachineLoading = computed(() => store.getters["machines/isLoading"]);
-const filtredMachines = computed(() => {
-  if (!searchParam.value) {
-    return machines.value;
-  }
-  return machines.value.filter((m) => {
-    return (
-      m.id.toString().startsWith(searchParam.value) ||
-      m.name.toLowerCase().startsWith(searchParam.value.toLowerCase()) ||
-      m.description.toLowerCase().startsWith(searchParam.value.toLowerCase())
-    );
-  });
-});
-
-const defectForm = ref<any>();
-const defectDialog = ref(false);
-const addDefectDto = ref<AddDefectDto>({
-  machineId: "",
-  name: "",
-  description: "",
-});
-
-const defectsHeaders: any = [
-  { title: "Имя", value: "name" },
-  { title: "Описание", value: "description" },
-  {
-    title: "Решён",
-    value: "isResolved",
-    key: "isResolved",
-    class: "table-header",
-  },
-  {
-    title: "Дата создания",
-    value: "createdAt",
-    key: "createdAt",
-  },
-  {
-    title: "Дата последнего обновления",
-    value: "updatedAt",
-    key: "updatedAt",
-  },
-  {
-    title: "Actions",
-    value: "actions",
-    key: "actions",
-  },
-];
-const refreshDefects = () => {
-  store.dispatch("defects/refresh");
-};
-
-const closeDefectDialog = () => {
-  defectDialog.value = false;
-  addDefectDto.value = { name: "", description: "", machineId: "" };
-};
-
-const openDefectDialog = (machineId: string) => {
-  defectDialog.value = true;
-  addDefectDto.value.machineId = machineId;
-};
-
-const addDefect = async () => {
-  if (!(await defectForm.value?.validate()).valid) {
-    return;
-  }
-
+onMounted(async () => {
+  isLoading.value = true;
   try {
-    const response = await api.defects.add(addDefectDto.value);
-
-    const realMachine = machines.value.find(
-      (m) => m.id === addDefectDto.value.machineId
-    );
-    store.commit("machines/replaceMachines", {
-      ...realMachine,
-      defects: [...realMachine?.defects, response.data],
-    });
-    closeDefectDialog();
-    store.commit("snackbar/showSnackbarSuccess", {
-      message: "Дефект успешно добавлен",
-    });
+    await store.dispatch("machines/fetchByDepartamentId", id.value);
   } catch {
     store.commit("snackbar/showSnackbarError", {
-      message: "Произошла ошибка при добавление дефекта",
+      message: "Произошла ошибка при запросе станков",
     });
+  } finally {
+    isLoading.value = false;
   }
-};
-
-const closeDefect = async (machineId: string, defectId: string) => {
-  try {
-    const updateDefectDto: UpdateDefectDto = {
-      ...defects.value.find((d) => d.id === defectId),
-      id: machineId,
-      isResolved: true,
-    };
-
-    console.log(updateDefectDto);
-    await api.defects.update(updateDefectDto);
-
-    const realMachine = machines.value.find((m) => m.id === machineId);
-
-    if (!realMachine) {
-      throw new Error();
-    }
-
-    store.commit("machines/replaceMachines", {
-      ...realMachine,
-      defects: [
-        ...realMachine?.defects.map((d) => {
-          if (d.id === defectId) {
-            d.isResolved = true;
-          }
-          return d;
-        }),
-      ],
-    });
-    store.commit("snackbar/showSnackbarSuccess", {
-      message: "Дефект успешно устранён",
-    });
-  } catch (e) {
-    console.log(e);
-    store.commit("snackbar/showSnackbarError", {
-      message: "Произошла ошибка при устранение дефекта",
-    });
-  }
-};
-
-const defects = computed(() => {
-  if (!selectedMachine.value) {
-    return [];
-  }
-
-  return machines.value?.find((m) => m.id === selectedMachine.value)?.defects;
 });
-const isDefectLoading = computed(() => store.getters["defects/isLoading"]);
-const isAddDefectLoading = computed(
-  () => store.getters["defects/isAddLoading"]
-);
+
+const machines = computed<Machine[]>(() => store.getters["machines/machines"]);
+const filtredMachines = computed<Machine[]>(() => {
+  let currMachines = machines.value;
+  if (selectedState.value) {
+    currMachines = currMachines.filter(
+      (m) => m.isActive === !!selectedState.value
+    );
+  }
+
+  if (searchParam.value) {
+    currMachines = currMachines.filter((m) => {
+      return (
+        m.name.toLowerCase().startsWith(searchParam.value.toLowerCase()) ||
+        m.description
+          .toLowerCase()
+          .startsWith(searchParam.value.toLowerCase()) ||
+        formatDateTime(m.createdAt).startsWith(searchParam.value) ||
+        formatDateTime(m.updateAt).startsWith(searchParam.value)
+      );
+    });
+  }
+
+  return currMachines;
+});
 </script>
+
+<style scoped></style>
