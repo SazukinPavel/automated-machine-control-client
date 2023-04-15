6ff98ef79034d445b4f2979fee20d56005e24936
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-16 00:22:36 +0300

    add confitm dialog

---
+++ src/components/confitmDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <slot></slot>
+  <v-dialog max-width="600" :model-value="modelValue">
+    <v-card class="py-10 px-5">
+      <v-card-title class="text-wrap">{{ props.message }}</v-card-title>
+      <div class="d-flex justify-center mt-4">
+        <v-btn
+          class="mx-2"
+          variant="outlined"
+          @click="emit('update:modelValue', false)"
+          >Отмена</v-btn
+        >
+        <v-btn class="mx-2" variant="outlined" @click="emit('confirm')"
+          >Подтвердить</v-btn
+        >
+      </div>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  message: { type: String },
+  modelValue: { type: Boolean, required: true },
+});
+const emit = defineEmits(["confirm", "update:modelValue"]);
+</script>
+
+<style scoped></style>
