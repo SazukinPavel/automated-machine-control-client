ecdea4a5e1274b18c6b6616ec3565c3f63d7bf04
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-16 20:00:01 +0300

    add password input

---
+++ src/components/ui/passwordInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-text-field
+    :model-value="props.value"
+    @input="emit('update:value', $event.target.value)"
+    :append-inner-icon="isShow ? 'mdi-eye' : 'mdi-eye-off'"
+    :rules="rules"
+    :type="isShow ? 'text' : 'password'"
+    :label="label"
+    @click:append-inner="isShow = !isShow"
+  ></v-text-field>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  value: { type: String, required: true },
+  rules: { type: Array, default: () => [], required: true },
+  label: { type: String, default: "" },
+});
+const emit = defineEmits(["update:value"]);
+
+const isShow = ref(false);
+</script>
+
+<style scoped></style>
