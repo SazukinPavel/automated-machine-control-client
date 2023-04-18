d44f8e07c1da15a80ecd2c3ca1a6cd6826b7ee11
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-18 19:00:01 +0300

    add btn add

---
+++ src/components/ui/addBtn.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-btn class="mt-1" append-icon="mdi-plus" :to="props.to">Добавить </v-btn>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps({ to: {} });
+</script>
+
+<style scoped></style>
