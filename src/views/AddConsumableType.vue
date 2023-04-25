f67227ae591dd506f9508cd784832efeaad91eae
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-25 22:30:14 +0300

    add consumable type views

---
+++ src/views/AddConsumableType.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-container
+    v-if="!isEditLoading"
+    style="max-width: 1000px"
+    class="pa-4 ma-auto mt-xl-15"
+  >
+    <v-card-title class="text-center">
+      {{
+        isEdit ? "Редактирование типа материалы" : "Добавление типа материала"
+      }}</v-card-title
+    >
+    <v-form ref="typeForm">
+      <v-text-field
+        class="my-2"
+        :rules="[requiredRule]"
+        label="Название"
+        v-model="addTypeDto.name"
+      />
+    </v-form>
+    <div class="d-flex justify-end align-center">
+      <v-btn class="mx-5" :disabled="isLoading" @click="goBack">Назад</v-btn>
+      <v-btn class="mx-5" :loading="isLoading" @click="addType">{{
+        props.isEdit ? "Обновить" : "Добавить"
+      }}</v-btn>
+    </div>
+  </v-container>
+  <loading v-else />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, defineProps } from "vue";
+import { useRoute } from "vue-router";
+import useValidators from "@/hooks/useValidators";
+import { useStore } from "vuex";
+import useNavigateTo from "@/hooks/useNavigateTo";
+import api from "@/api";
+import Loading from "@/components/loading.vue";
+import AddConsumableTypeDto from "@/types/dto/consumable-types/AddConsumableTypeDto";
+import UpdateConsumableTypeDto from "@/types/dto/consumable-types/UpdateConsumableTypeDto";
+
+const props = defineProps({ isEdit: { type: Boolean, default: false } });
+
+const route = useRoute();
+const store = useStore();
+const { requiredRule } = useValidators();
+const { goTo, goBack } = useNavigateTo();
+
+const addTypeDto = ref<AddConsumableTypeDto>({});
+const typeForm = ref<any | null>(null);
+const isLoading = ref(false);
+const isEditLoading = ref(false);
+
+const addType = async () => {
+  if (!(await typeForm.value?.validate()).valid) {
+    return;
+  }
+
+  isLoading.value = true;
+
+  try {
+    if (props.isEdit) {
+      const dto = { ...addTypeDto.value } as UpdateConsumableTypeDto;
+      await store.dispatch("consumableTypes/update", dto);
+    } else {
+      await store.dispatch("consumableTypes/add", {
+        ...addTypeDto.value,
+      });
+    }
+
+    store.commit("snackbar/showSnackbarSuccess", {
+      message: `Тип материала успешно ${props.isEdit ? "изменён" : "добавлен"}`,
+    });
+    goTo("ConsumableTypes");
+  } catch (e: any) {
+    console.log(e);
+    store.commit("snackbar/showSnackbarError", {
+      message:
+        e.response.data.message ||
+        `Произошла ошибка при ${
+          props.isEdit ? "изменение" : "добавление"
+        } типа материала`,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(async () => {
+  if (props.isEdit) {
+    isEditLoading.value = true;
+    try {
+      const response = await api.consumableTypes.getById(
+        route.params.id.toString()
+      );
+      addTypeDto.value = { ...response.data };
+    } catch {
+      store.commit("snackbar/showSnackbarSuccess", {
+        message: "Произошла ошибка при получение типа материала",
+      });
+      goBack();
+    } finally {
+      isEditLoading.value = false;
+    }
+  }
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/ConsumableTypes.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-if="!isLoading">
+    <v-row class="ma-4" dense="dense">
+      <v-col style="min-width: 150px" cols="3">
+        <search v-model="searchParam" />
+      </v-col>
+      <v-col>
+        <add-btn :to="{ name: 'AddConsumableType' }" />
+      </v-col>
+    </v-row>
+    <consumable-type-card
+      v-for="type in filtredTypes"
+      :key="type.id"
+      :type="type"
+    />
+  </div>
+  <loading v-else></loading>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import { useStore } from "vuex";
+import Loading from "@/components/loading.vue";
+import Search from "@/components/search.vue";
+import AddBtn from "@/components/ui/addBtn.vue";
+import ConsumableType from "@/types/busnes/ConsumableType";
+import ConsumableTypeCard from "@/components/consumableTypeCard.vue";
+
+const store = useStore();
+
+const searchParam = ref("");
+const isLoading = ref(false);
+
+const types = computed<ConsumableType[]>(
+  () => store.getters["consumableTypes/types"]
+);
+const filtredTypes = computed<ConsumableType[]>(() => {
+  if (!searchParam.value) {
+    return types.value;
+  }
+  return types.value.filter((d) =>
+    d.name.toLowerCase().includes(searchParam.value.toLowerCase())
+  );
+});
+
+onMounted(async () => {
+  isLoading.value = true;
+  try {
+    await store.dispatch("consumableTypes/fetch");
+  } catch {
+    store.commit("snackbar/showSnackbarError", {
+      message: "Произошла ошибка при запросе типов материалов",
+    });
+  } finally {
+    isLoading.value = false;
+  }
+});
+</script>
