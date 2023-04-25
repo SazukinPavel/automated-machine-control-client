2106d21904b3c22bc45f39b6f714746af6aee152
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-25 20:31:44 +0300

    add year rule & change api

--- src/hooks/useValidators.ts
+++ src/hooks/useValidators.ts
@@ -2,7 +2,15 @@ import { useRouter } from "vue-router";
 import { ref } from "vue";
 
 export default function useValidators() {
-  const required = ref((val: string) => !!val || "Поле обязательное");
+  const requiredRule = ref((val: string) => !!val || "Поле обязательное");
 
-  return { required };
+  const yearRule = ref((val: number) => {
+    if (val > 3000 || val < 1900) {
+      return "Некоректный год";
+    }
+
+    return true;
+  });
+
+  return { requiredRule, yearRule };
 }
