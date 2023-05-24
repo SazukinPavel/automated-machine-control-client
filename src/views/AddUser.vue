75a86e52416d4ff2c91c58a18a5fb495cddcba7b
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-24 11:42:33 +0300

    add role & pass

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -13,6 +13,19 @@
         color="primary"
         v-model="addUserDto.login"
       />
+      <password-input
+        label="Пароль"
+        :rules="[requiredRule]"
+        v-model="addUserDto.password"
+      />
+      <v-select
+        label="Роль"
+        :rules="[requiredRule]"
+        :items="roles"
+        item-title="title"
+        item-value="value"
+        v-model="addUserDto.role"
+      />
     </v-form>
     <div class="d-flex justify-end align-center">
       <v-btn class="mx-5" :disabled="isLoading" @click="goBack">Назад</v-btn>
@@ -28,6 +41,7 @@ import useNavigateTo from "@/hooks/useNavigateTo";
 import { ref, onMounted } from "vue";
 import AddUserDto from "@/types/dto/users/AddUserDto";
 import PageTitle from "@/components/ui/pageTitle.vue";
+import PasswordInput from "@/components/ui/passwordInput.vue";
 
 const store = useStore();
 const { requiredRule } = useValidators();
@@ -35,7 +49,11 @@ const { goBack, goTo } = useNavigateTo();
 
 const isLoading = ref(false);
 const userForm = ref<any | null>(null);
-const addUserDto = ref<AddUserDto>({ login: "", role: "user" });
+const addUserDto = ref<AddUserDto>({ login: "", role: "user", password: "" });
+const roles = ref([
+  { title: "Админ", value: "admin" },
+  { title: "Работник", value: "user" },
+]);
 
 const add = async () => {
   if (!(await userForm.value?.validate()).valid) {
@@ -46,7 +64,6 @@ const add = async () => {
   try {
     await store.dispatch("users/add", {
       ...addUserDto.value,
-      password: addUserDto.value.login,
     });
     store.commit("snackbar/showSnackbarSuccess", {
       message: `Ответсвенный успешно добавлен`,
