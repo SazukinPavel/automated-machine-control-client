09edf1efea20abfd3b4e49f4befc3595521e393e
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-06-01 12:24:03 +0300

    update consumables module

--- src/api/modules/consumables.ts
+++ src/api/modules/consumables.ts
@@ -2,6 +2,7 @@ import Api from "@/api/api";
 import BaseApiModule from "@/types/utils/BaseApiModule";
 import AddConsumableDto from "@/types/dto/consumables/AddConsumableDto";
 import Consumable from "@/types/busnes/Consumable";
+import UpdateConsumableDto from "@/types/dto/consumables/UpdateConsumableDto";
 
 export default class ConsumablesModule extends BaseApiModule {
   constructor(api: Api) {
@@ -12,11 +13,19 @@ export default class ConsumablesModule extends BaseApiModule {
     return this.get<Consumable[]>("", { params });
   }
 
+  getById(id: string) {
+    return this.get<Consumable>(`/${id}`);
+  }
+
   add(dto: AddConsumableDto) {
     return this.post<AddConsumableDto, Consumable>("", dto);
   }
 
   drop(id: string) {
     return this.delete(`/${id}`);
+  }
+
+  update(dto: UpdateConsumableDto) {
+    return this.put<UpdateConsumableDto, Consumable>("", dto);
   }
 }
